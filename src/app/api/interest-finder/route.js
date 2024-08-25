38bf9bec32a14d8a937279a75e933848440d6fa9
nafimulyoo <rxnafimulyo@gmail.com>
2024-08-25 23:35:43 +0700

    Fix deployment auth issues

--- src/app/api/interest-finder/route.js
+++ src/app/api/interest-finder/route.js
@@ -8,6 +8,7 @@ export async function POST(request) {
     const { message } = await request.json();
 
     const vertex_ai = new VertexAI({ project: '972945849581', location: 'us-central1', googleAuthOptions: {
+      projectId: process.env.GCP_PROJECT_ID,
       credentials: {
           client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
           private_key: process.env.GCP_PRIVATE_KEY
